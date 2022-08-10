2e400c5e84309ceefc19260f62362c4f346c2cfb
Diego L. Guarin <diego.guarinlopez@mail.mcgill.ca>
2022-08-10 16:31:37 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navigation
+        // you can use your own router's api to get pathname
+        activeItemId="/management/members"
+        onSelect={({itemId}) => {
+          // maybe push to the route
+        }}
+        items={[
+          {
+            title: 'Dashboard',
+            itemId: '/dashboard',
+            // you can use your own custom Icon component as well
+            // icon is optional
+            elemBefore: () => <Icon name="inbox" />,
+          },
+          {
+            title: 'Management',
+            itemId: '/management',
+            elemBefore: () => <Icon name="users" />,
+            subNav: [
+              {
+                title: 'Projects',
+                itemId: '/management/projects',
+              },
+              {
+                title: 'Members',
+                itemId: '/management/members',
+              },
+            ],
+          },
+          {
+            title: 'Another Item',
+            itemId: '/another',
+            subNav: [
+              {
+                title: 'Teams',
+                itemId: '/management/teams',
+              },
+            ],
+          },
+        ]}
+      />
+  </>
+);
 }
 
 export default App;
